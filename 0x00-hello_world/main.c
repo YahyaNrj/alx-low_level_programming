08148dfc76d76f7cb4fec5cf3e872dfdbd1ea53d
YahyaNrj <yahyabachr@gmail.com>
2023-06-16 16:52:44 -0700

    le passer c le passer

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+julien@ubuntu:~/c/0x00$ ./100-intel 
+julien@ubuntu:~/c/0x00$ cat main.s
+    .file   "main.c"
+    .intel_syntax noprefix
+    .text
+    .globl  main
+    .type   main, @function
