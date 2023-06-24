ffb30fe4c7807560b06b63ce9f8e8970c211a718
YahyaNrj <yahyabachr@gmail.com>
2023-06-23 18:10:52 -0700

    wi3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,2 @@
-for num in range(1, 101):
-    output = ""
-    if num % 3 == 0:
-        output += "Fizz "
-    if num % 5 == 0:
-        output += "Buzz "
-    if output == "":
-        output = str(num)
-    print(output, end=" ")
+#include "main.h"
 
-print()
